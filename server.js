ed8fd2fb8dafbe8e9bd44fde1cc30f331e0304ec
Israel Sanchez <Israel.pro1994@gmail.com>
2021-01-23 18:56:35 -0600

    created config folder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => res.send('This is the API and it. Is. RUNNING!!'));
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001;
 
 app.listen( PORT, () => console.log(`Server started on ${PORT}`));
 
