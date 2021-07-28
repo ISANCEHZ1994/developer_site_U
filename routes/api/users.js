818e32f9b7133cfe3780e3f9a80de5e1c003e5dd
Israel Sanchez <isanchez@atser.com>
2021-07-28 11:49:37 -0500

    users can now have jsonWebToken

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config');
+const config = require('config'); // will get the config folder
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../Models/User');
@@ -13,7 +13,7 @@ const User = require('../../Models/User');
 // @access  Public => if you need a token to access a SPECIFC route 
 // ex: to add profile you need to be authenticated => send a token to that route
 
-router.post('/',[ // within the route we add another parameter - array of things to check for!
+router.post('/',[   // within the route we add another parameter - array of things to check for!
     check('name', 'NOTE: Name is required!') 
     // checking to make sure the name is there if not returns custom error message
     .not()  // not and isEmpty are requried when using check!
@@ -28,7 +28,7 @@ router.post('/',[ // within the route we add another parameter - array of things
 ], async (req,res) => {
     const errors = validationResult(req);
     // we want our custom error messages to appear when its checking for something 
-    if(!errors.isEmpty()){// if there ARE errors we want a response!
+    if(!errors.isEmpty()){  // if there ARE errors we want a response!
         return res.status(400).json({ errors: errors.array() }); 
     }
     // Destructor so that we don't always call it again and again
@@ -42,12 +42,12 @@ router.post('/',[ // within the route we add another parameter - array of things
         };
 
         // get users gravater
-        const avatar = gravatar.url(email, { // adding defaults
+        const avatar = gravatar.url(email, {    // adding defaults
             s: '200',
             r: 'pg',
             d: 'mm'
         });
-        user = new User({ // creates new instance but does not SAVE the 'new' user
+        user = new User({ // creates NEW instance but does not SAVE the 'new' user
             name, 
             email,
             avatar,
@@ -55,34 +55,35 @@ router.post('/',[ // within the route we add another parameter - array of things
         })
 
         // encrypt password
-        const salt = await bcrypt.genSalt(10); // this is for security - the more you have the slower it can be
+        const salt = await bcrypt.genSalt(10);  // this is for security - the more you have the slower it can be
         user.password = await bcrypt.hash(password, salt); // now we are saying we want to encrypt the password
         await user.save();
 
         // return jsonwebtoken  
         const payload = {
-            user:{
+            user: {
                 id: user.id
             }
         };
 
-        jwt.sign(payload, 
-            config.get('jwtToken'),
-            { expiresIn: 360000 }, // time till logout is optional
-            (err, token) => {
-                if(err){
-                    res.json({ token });
-                }
+        jwt.sign(
+            payload,    // use the newly created jsonwebtoken - literally look up a few lines
+            config.get('jwtSecret'), // from the config folder it'll find and use jwtSecret
+            { expiresIn: 360000 }, // time till logout is optional - in this case an hour
+            (err, token) => {      // a callback that takes 2 arguments
+               if (err) throw err; 
+               res.json({ token }); // we want to see the token created!
             }
         );
 
-        res.send('user registered! => we confirmed that order cheif'); 
+        res.send('user registered! => we confirmed that order chief'); 
+
      } catch (error) {
 
         console.error(error);
-        res.status(500).send("SERVER ERROR");
+        res.status(500).send("SERVER ERROR - perhaps you done goofed?");
          
      }; // ends try block here!
-});
+}); // router.post ends here!
 
 module.exports = router;
