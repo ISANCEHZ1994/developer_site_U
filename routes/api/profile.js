fbfb1a4925f6875523ad798abcf9b2e0aec8c55d
Israel Sanchez <isanchez@atser.com>
2021-07-28 16:49:36 -0500

    creating profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../Middleware/auth');
 
-// @route   GET api/profile
-// @desc    Test Route
-// @access  Public
-router.get('/', (req,res) => res.send('profile route'));
+const Profile = require('../../Models/Profile');
+const User = require('../../Models/User');
+
+// @route   GET api/profile/me
+// @desc    Get Current Users Profile
+// @access  Private
+
+router.get('/me', auth, async (req,res) => {
+
+    try{
+        // the user: passing thru is going to pertain to our profile model user field
+        // populate with the name of the user and the avatar => from the user model
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
+            'user',
+            ['name', 'avatar']
+        ); 
+
+        if(!profile){
+            return res.status(400).json({ msg: 'There is no profile for this user' })
+        }
+
+        res.json(profile);
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('SERVER ERROR - yo git good foo');
+    }
+
+});
+
+
 
 module.exports = router;
