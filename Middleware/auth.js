5b17ef73902266eecf8d4639dbce7a2a181e7e66
Israel Sanchez <isanchez@atser.com>
2021-07-28 13:40:38 -0500

    created custom middleware

---
+++ Middleware/auth.js
@@ -0,0 +1,27 @@
+const  jwt = require('jsonwebtoken');
+const config = require('config');
+
+// BELOW IS OUR CUSTOM MIDDLEWARE
+
+module.exports = function(req, res, next){
+
+    // Get Token from header
+    const token = req.header('x-auth-token');
+
+    // Check if not Token
+    if(!token) {
+        return res.status(401).json({ msg: 'No Token, Authorization DENIED' });
+    }
+
+    // Verify Token
+    try{
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user; // this the decoded object
+        next();
+
+    } catch(err){
+        res.status(401).json({ msg: 'Token is not vaild' });
+    }
+
+};
