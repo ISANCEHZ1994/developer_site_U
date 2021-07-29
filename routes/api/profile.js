36dc27d5b88414c699bb9a5cc8de9b8defad91ca
Israel Sanchez <isanchez@atser.com>
2021-07-29 09:17:15 -0500

    can now get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -12,26 +12,29 @@ const User = require('../../Models/User');
 router.get('/me', auth, async (req,res) => {
 
     try{
-        // the user: passing thru is going to pertain to our profile model user field
+        // the user: passing thru is going to pertain to our profile model user field => ProfileSchema userID
         // populate with the name of the user and the avatar => from the user model
-        const profile = await Profile.findOne({ user: req.user.id }).populate(
-            'user',
-            ['name', 'avatar']
+        const profile = await Profile.findOne({ user: req.user.id }).populate( //   populate from 'USER'
+            'user', 
+            ['name', 'avatar'] // array of fields that we want to bring in from user
         ); 
 
-        if(!profile){
-            return res.status(400).json({ msg: 'There is no profile for this user' })
+        if(!profile){ // if there ISN'T a profile
+            return res
+            .status(400)
+            .json({ msg: 'Sorry but here is no profile for this user - MAI GAI' })
         }
 
-        res.json(profile);
+        res.json(profile); // if there is a profile we want to see it!
 
     }catch(err){
         console.error(err.message);
-        res.status(500).send('SERVER ERROR - yo git good foo');
+        res
+        .status(500)
+        .send('SERVER ERROR - yo git good foo');
     }
 
 });
-
 
 
 module.exports = router;
