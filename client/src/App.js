ce2f446e81b903900e02c7ab2618dac57f0519ae
Israel Sanchez <Israel.pro1994@gmail.com>
2021-10-11 09:55:08 -0500

    frontend work

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './Components/Layout/NavBar';
+import Landing from './Components/Layout/Landing';
+import Login from './Components/Auth/Login';
+import Register from './Components/Auth/Register';
 import './App.css';
 
 // automaticlly returns
 const App = () => (
-  <Fragment>
-    <h1> this is HERE </h1>
-  </Fragment>
-)
+  <Router>
+    <Fragment>
+      <NavBar/>
+      <Route exact path= '/' component={ Landing }/>
+      <section className= "container">
+        <Switch>
+            <Route exact path= '/login' component={ Login }/>
+            <Route exact path= '/register' component={ Register }/>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
+);
 
-// below will work but you HAVE to return to show 
-// const App = () => {
-// <Fragment>
-//   <h1> this is HERE </h1>
-// </Fragment>
-// }
 
 export default App;
+
