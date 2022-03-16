67ed04c0abafbd75fd49ef6f2329654b2eac07e1
Israel Sanchez <Israel.pro1994@gmail.com>
2022-03-16 16:51:45 -0500

    organized component props

--- client/src/Components/Profiles/ProfileItem.js
+++ client/src/Components/Profiles/ProfileItem.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-const ProfileItem = ({ profile: {
-                          user: { _id, name, avatar },
-                          status,
-                          company,
-                          location,
-                          skills 
-                        } 
+const ProfileItem = ({ 
+  profile: {
+    user: { _id, name, avatar },
+    status,
+    company,
+    location,
+    skills 
+  } 
 }) => {
 
   return (    
